067c28d34551bcb83ef1924ec5fe9dfc454a76a3
Heráclito R. Carvalho <122533608+heraclitoRcarvalho@users.noreply.github.com>
2024-07-22 08:53:56 -0300

    Create conversor_de_temperatura.py

---
+++ conversor_de_temperatura.py
@@ -0,0 +1,12 @@
+#Conversor de temperatura de °F para °C
+
+# farin = float(input('Digite a temperatura em °F:  '))
+# celsius = (farin - 32) * 5//9
+# print(f'A temperatura de {farin} em °C é : {celsius}')
+
+#Conversor de temperatura de °C para °F
+
+celsius = float(input('Coloque a tempertura em °C: '))
+farin = (celsius * 9//5) + 32
+print(f'A temperatura de {celsius} em °F é : {farin}')
+ 
